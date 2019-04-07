b4fe758e654cbcdb85d38ecd40768b7f5546c1a5
FS <ansanchez4@poligran.edu.co>
2019-04-07 14:24:31 -0500

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistroComponent } from './Sesion/registro/registro.component';
+import { ArtistasComponent } from './Home/artistas/artistas.component';
+import { LoginComponent } from './Sesion/login/login.component';
+import { PerfilComponent } from './Artista/perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: ArtistasComponent },
+  { path: 'perfil/:idArtista', component: PerfilComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
